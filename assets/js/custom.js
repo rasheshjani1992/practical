8cc6872b847ffb710d8098f5bc01f575438ac7dc
rasheshjani1992 <137692863+rasheshjani1992@users.noreply.github.com>
2023-06-25 16:32:28 +0530

    Practical submit

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+  //-------   	Menu Js Start   	-------//
+ 	
+  document.querySelector('#toggle-btn').onclick = function (e) {
+    var nav = document.querySelector('.header-section');
+    
+    nav.classList.toggle('show');
+    e.preventDefault();
+  }
+
+  //-------   	Menu Js End   	-------//
+
